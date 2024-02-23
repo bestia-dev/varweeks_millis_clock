ed84087317715c2e317530f56fb8ee6db5b50c96
luciano_bestia <info@bestia.dev>
2024-02-23 12:01:20 -0500

    2024-02-23

--- automation_tasks_rs/src/main.rs
+++ automation_tasks_rs/src/main.rs
@@ -115,7 +115,7 @@ fn completion() {
 
 /// cargo build
 fn task_build() {
-    //let cargo_toml = CargoToml::read();
+    //let cargo_toml = cl::CargoToml::read();
     cl::auto_version_increment_semver_or_date();
     cl::run_shell_command("cargo fmt");
     cl::run_shell_command("wasm-pack build --target web");
@@ -137,7 +137,7 @@ fn task_build() {
 
 /// cargo build --release
 fn task_release() {
-    //let cargo_toml = CargoToml::read();
+    //let cargo_toml = cl::CargoToml::read();
     cl::auto_version_increment_semver_or_date();
     cl::auto_cargo_toml_to_md();
     cl::auto_lines_of_code("");
@@ -233,7 +233,7 @@ r#"
 fn task_publish_to_web_server() {
     println!(r#"{YELLOW}Use ssh-agent and ssh-add to store passphrase for ssh.{RESET}"#);
 
-    let cargo_toml = CargoToml::read();
+    let cargo_toml = cl::CargoToml::read();
     // git tag
     let shell_command = format!(
         "git tag -f -a v{version} -m version_{version}",
